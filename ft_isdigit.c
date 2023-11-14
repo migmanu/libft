d772b128b498f422fa1a5dbecaaf9a25430cc07b
migmanu <jmanuelmigoya@gmail.com>
2023-11-14 20:50:31 +0100

    Correct return value

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,17 +6,18 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 10:43:10 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/05/15 14:11:54 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/11/14 20:49:33 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// Returns 0 is c is digit
 int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
+		return (0);
+	return (1);
 }
 /*
 int	main(void)
