6b6f8517a746d2ea7e96538cc9693579cde28256
migmanu <jmanuelmigoya@gmail.com>
2023-11-14 20:54:42 +0100

    Testing

--- ft_str_is_nbr.c
+++ ft_str_is_nbr.c
@@ -6,7 +6,7 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 20:46:14 by jmigoya-          #+#    #+#             */
-/*   Updated: 2023/11/14 20:49:50 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2023/11/14 20:54:24 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_str_is_nbr(const char *str)
 {
 	int	i;
 
-	if (!str)
+	if (!str || str[0] == '\0')
 		return (-1);
 	i = 0;
 	while (str[i] != '\0')
@@ -28,3 +28,10 @@ int	ft_str_is_nbr(const char *str)
 	}
 	return (0);
 }
+/*
+int	main(void)
+{
+	char *str = "asdf";
+	printf("%d\n", ft_str_is_nbr(str));
+	return (0);
+}*/
